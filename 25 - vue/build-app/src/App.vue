6b8c6a27c94f50d41f4e38119448f199c7dfd1ab
1065432936 <1065432936@qq.com>
2017-03-28 18:00:18 +0800

    changes

---
+++ 25 - vue/build-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+
+  export default {
+    data() {
+      return {
+        quotes: [
+          'Just a Quote to see something'
+        ],
+        maxQuotes: 10
+      }
+    },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote);
+      }
+    },
+    components: {
+      'app-quote-grid': QuoteGrid,
+      'app-new-quote': NewQuote,
+    }
+  }
+</script>
